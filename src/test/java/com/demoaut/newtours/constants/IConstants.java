bd78ac3165ac420e27b9e6c58e4e5ec5a029a64c
rohan <rohan@192.168.1.14>
2019-06-30 21:39:14 -0500

    chromedriver in mac

--- src/test/java/com/demoaut/newtours/constants/IConstants.java
+++ src/test/java/com/demoaut/newtours/constants/IConstants.java
@@ -2,7 +2,7 @@ package com.demoaut.newtours.constants;
 
 public interface IConstants {
 
-	public static final String CHROMEDRIVERPATH = "test-artifacts//Drivers//Windows//chromedriver.exe";
+	public static final String CHROMEDRIVERPATH = "test-artifacts//Drivers//Mac//chromedriver";
 	public static final int IMPLICITWAITSTND=30;
 	public static final String CHROME = "CHROME";
 	
