83a305b4109a327e14534e1c72210c7e51487de9
manasabalagoni <49225717+manasabalagoni@users.noreply.github.com>
2019-09-16 11:23:49 +0530

    1

--- src/test/java/com/demoaut/newtours/constants/IConstants.java
+++ src/test/java/com/demoaut/newtours/constants/IConstants.java
@@ -2,7 +2,7 @@ package com.demoaut.newtours.constants;
 
 public interface IConstants {
 
-	public static final String CHROMEDRIVERPATH = "/home/vagrant/.jenkins/workspace/NewtoursTest/chromedriver";
+	public static final String CHROMEDRIVERPATH = "/home/vagrant/.jenkins/workspace/NewtoursTest/chromedriver_linux64.zip";
 	public static final int IMPLICITWAITSTND=30;
 	public static final String CHROME = "CHROME";
 	
